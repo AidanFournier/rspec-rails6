503e76c47dc7c5f136ef75e9cf1a33f201fbcdd6
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-11-01 22:22:11 +0900

    Created test outlines for post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  current_user = User.find_or_create!(email: 'aidan@example.com', password: '111111', password_confirmation: '111111')
+
+  it 'has a title' do
+  end
+
+  it 'has a body' do
+  end
+
+  it 'has a title at least 2 characters long' do
+  end
+
+  it 'has a body between 5 adn 100 characters' do
+  end
+
+  it 'has numerical views' do
+  end
+
 end

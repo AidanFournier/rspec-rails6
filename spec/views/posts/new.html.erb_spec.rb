f466b87cf2f10667221dd076623edd8ee35c06f2
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2021-11-02 23:56:32 +0900

    Fixed failure on new spec page

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "posts/new", type: :view do
+current_user = User.first_or_create!(email: 'aidan@example.com', password: '111111', password_confirmation: '111111')
+
   before(:each) do
     assign(:post, Post.new(
       title: "MyString",
       body: "MyText",
-      user: nil,
+      user: current_user,
       views: 1
     ))
   end
@@ -18,10 +20,6 @@ RSpec.describe "posts/new", type: :view do
       assert_select "input[name=?]", "post[title]"
 
       assert_select "textarea[name=?]", "post[body]"
-
-      assert_select "input[name=?]", "post[user_id]"
-
-      assert_select "input[name=?]", "post[views]"
     end
   end
 end
